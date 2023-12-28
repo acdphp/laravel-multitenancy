871ce396a7f0579ba208ea5f7c6ab344e0e8ca5c
Carlo D <carlo.dinopol@gmail.com>
2023-12-28 10:50:02 +0100

    temporary fix for sqllite migration glitch

--- workbench/database/migrations/2023_12_28_082921_alter_users_add_company_id_column.php
+++ workbench/database/migrations/2023_12_28_082921_alter_users_add_company_id_column.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('company_id')->nullable();
         });
     }
 
